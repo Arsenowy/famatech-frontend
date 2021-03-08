fe26acd9d859c3aa2cc909054d0189c94e994d56
arsenowski <mkkrawczyk00@gmail.com>
2021-03-08 21:48:15 +0100

    Add darkmode to modal windows

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,7 @@ function darkMode() {
     document.documentElement.style.setProperty("--color-dark", "#CCC");
     document.documentElement.style.setProperty("--color-black", "#fff");
     document.documentElement.style.setProperty("--color-grey", "#fff");
-    document.querySelector(".nav__logo--img").src="src/images/logo-bookmark-white.png";
+    document.querySelector(".nav__logo--img").src = "src/images/logo-bookmark-white.png";
 }
 
 function lightmode() {
@@ -17,38 +17,20 @@ function lightmode() {
 }
 
 var toggleButton = document.querySelector("#switch-sm");
-// if (toggleButton.checked == true) {
-//     darkMode();
-// }
+
+function changeCloseButton() {
+    modals = document.querySelectorAll(".btn__close");
+    modals.forEach(el => {
+        el.classList.toggle('btn__light');
+        el.classList.toggle('btn__dark');
+    });
+}
 
 var t = false;
 toggleButton.addEventListener('click', e => {
     setTimeout(() => {
         t == false ? darkMode() : lightmode();
         t = !t;
+        changeCloseButton();
     }, 125);
 });
-
-
-// /////////////////////////////
-// var box = document.body;
-
-// function fadeOutIn(elem, speed ) {
-
-//     if (!elem.style.filter(brightness)) {
-//         elem.style.filter(brightness) = 1;
-//     } // end if
-
-//     var outInterval = setInterval(function() {
-//         elem.style.filter(brightness) -= 0.02;
-//         if (elem.style.filter(brightness) <= 0) {
-//             clearInterval(outInterval);
-//             var inInterval = setInterval(function() {
-//                 elem.style.filter(brightness) = Number(elem.style.filter(brightness))+0.02;
-//                 if (elem.style.filter(brightness) >= 1)
-//                     clearInterval(inInterval);
-//             }, speed/50 );
-//         } // end if
-//     }, speed/50 );
-
-// } // end fadeOut()
