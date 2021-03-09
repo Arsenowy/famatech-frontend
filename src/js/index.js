bdc6ce545a668bcbc5d39e2e27783240b857e4db
arsenowski <mkkrawczyk00@gmail.com>
2021-03-09 09:15:53 +0100

    Add darkmode and responsivness to qa section

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 function darkMode() {
     document.documentElement.style.setProperty("--color-darkmode", "#222");
     document.documentElement.style.setProperty("--color-darkmode-lite", "#333");
+    document.documentElement.style.setProperty("--color-darkmode-light", "#444");
     document.documentElement.style.setProperty("--color-dark", "#CCC");
     document.documentElement.style.setProperty("--color-black", "#fff");
     document.documentElement.style.setProperty("--color-grey", "#fff");
@@ -10,6 +11,7 @@ function darkMode() {
 function lightmode() {
     document.documentElement.style.setProperty("--color-darkmode", "#fff");
     document.documentElement.style.setProperty("--color-darkmode-lite", "#fff");
+    document.documentElement.style.setProperty("--color-darkmode-light", "#fff");
     document.documentElement.style.setProperty("--color-dark", "#333");
     document.documentElement.style.setProperty("--color-black", "#000");
     document.documentElement.style.setProperty("--color-grey", "#777");
